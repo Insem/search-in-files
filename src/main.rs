6f1525aa150c9c4bc612785654a1eb2c6c648461
Insem <600amper@gmail.com>
2024-03-17 13:25:26 +0300

    errors

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,60 @@
+use core::fmt::Error;
+
 use std::{
     fs::{self, File},
     io::{self, BufRead, BufReader},
     path::Path,
 };
-
 fn main() {
     println!("Hello, world!");
-    search_in_files(Path::new("./examples"), "вавпаавпвпвпвапвапв");
+    let arr = search_in_files(Path::new("./examples"), "Дети").unwrap();
+    println!("--Arr {:?}", arr);
 }
 
-fn search_in_files(dir: &Path, word: &str) -> io::Result<()> {
+fn search_in_files(dir: &Path, word: &str) -> io::Result<Vec<String>> {
+    println!("str:{:?}", &format!(r"{}(\W|$)", word));
+    let reg = regex::Regex::new(&format!(r"{}(\W|$)", word)).map_err(|e| {
+        std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!("Failed to build regex {:?}", format!(r"{}(\W|$)", word)),
+        )
+    })?;
+    let mut match_arr: Vec<String> = Vec::new();
     if dir.is_dir() {
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
             let path = entry.path();
             if path.is_dir() {
-                search_in_files(&path, word)?;
+                match_arr.append(search_in_files(&path, word)?.as_mut());
             } else {
-                read_file(&path, word);
-                //println!("File: {:?}", path);
-            }
-        }
-    }
-    Ok(())
-}
-
-fn read_file(filepath: &Path, word: &str) -> io::Result<()> {
-    let file = File::open(filepath)?;
-    let reader = BufReader::new(file);
-    println!("str:{:?}", &format!(r"{}(\W|$)", word));
-    let re = regex::Regex::new(&format!(r"{}(\W|$)", word)).unwrap();
-    for line in reader.lines() {
-        //println!("Liine {:?}", line);
-        let l = line.unwrap();
-        /*match l.find(word) {
-            Some(w) => {
-                let end_of_the_word = w + word.to_string().len() + 1;
-                println!(
-                    "Line:  {:?} {:?} {:?} {:?} {:?}",
-                    l.len(),
-                    end_of_the_word,
-                    end_of_the_word + 1,
-                    l.get(end_of_the_word..), //char::from_u32(l.bytes().nth(w)),
-                    w
-                );
-                let symbol_after_word = l.get(end_of_the_word..end_of_the_word + 1).unwrap();
-
-                if symbol_after_word.chars().nth(0).unwrap().is_alphabetic() {
-                    println!("Line: {:?}", w);
+                if match_file(&path, &reg)? {
+                    match_arr.push(path.into_os_string().into_string().map_err(|e| {
+                        std::io::Error::new(std::io::ErrorKind::Other, "Failed to get os file path")
+                    })?);
                 }
             }
-
-            None => println!(""),
-        };*/
-        if re.is_match(&l) {
-            println!("Found {:?}", filepath);
         }
     }
 
-    Ok(())
+    Ok(match_arr)
+}
+
+fn match_file(filepath: &Path, word: &regex::Regex) -> io::Result<bool> {
+    let file = File::open(filepath)?;
+    let reader = BufReader::new(file);
+    for _line in reader.lines() {
+        let line = if _line.is_ok() {
+            _line.unwrap()
+        } else {
+            println!("Err: failed to read line in {:?}", filepath);
+            continue;
+        };
+
+        if word.is_match(&line) {
+            println!("Found {:?}", filepath);
+            return Ok(true);
+        }
+    }
+
+    Ok(false)
 }
