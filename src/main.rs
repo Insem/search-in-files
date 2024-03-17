633aabb8600dc809c763458b196f2db367b25110
Insem <600amper@gmail.com>
2024-03-17 22:02:18 +0300

    pretty work

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
 use core::fmt::Error;
+use rocket::response::status::Custom;
+use rocket::serde::{json::Json, Serialize};
 
 use crate::file_search::FileSearch::search_in_files;
+use rocket::http::Status;
 use std::{
     fs::{self, File},
     io::{self, BufRead, BufReader},
     path::Path,
 };
 pub mod file_search;
-fn main() {
-    println!("Hello, world!");
-    let arr = search_in_files(Path::new("../examples"), "Дети").unwrap();
-    println!("--Arr {:?}", arr);
+#[macro_use]
+extern crate rocket;
+
+#[launch]
+fn server() -> _ {
+    rocket::build().mount("/", routes![index])
+}
+const DIR: &'static str = "examples";
+#[get("/search/files/<word>")]
+fn index(word: &str) -> Result<Json<Vec<String>>, Custom<String>> {
+    let arr = search_in_files(
+        Path::new(&format!(
+            "{}/{}",
+            std::env::current_dir()
+                .unwrap()
+                .as_os_str()
+                .to_str()
+                .unwrap(),
+            DIR
+        )),
+        word,
+    )
+    .map_err(|e| Custom(Status::ServiceUnavailable, e.to_string()))?;
+
+    Ok(Json(arr))
 }
